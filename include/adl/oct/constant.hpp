30a0176a0024ae9493936f91d5b23fbbece0c12c
Flávio Lisbôa <flavio.lisboa@fasolti.com.br>
2018-01-17 10:54:12 -0200

    Renamed the ValueType concept to ConstantType, fixed some imports in adl/oct/cpu (fix for last commit)

---
+++ include/adl/oct/constant.hpp
@@ -0,0 +1,23 @@
+// $flavio.lisboa @ 2018-01-17.
+//
+/*
+ * @file constant.hpp
+ */
+
+#ifndef adl__oct__constant__hpp__
+#define adl__oct__constant__hpp__
+
+#include "adl.cfg.hpp"
+#include "adl/oct.fwd.hpp"
+
+
+//
+// [[ API ]]
+//
+adl_BEGIN_MAIN_MODULE(oct)
+
+
+
+adl_END_MAIN_MODULE
+
+#endif // adl__oct__constant__hpp__
---
+++ include/adl/oct/var_id.hpp
@@ -0,0 +1,23 @@
+// $flavio.lisboa @ 2018-01-17.
+//
+/*
+ * @file var_id.hpp
+ */
+
+#ifndef adl__oct__var_id__hpp__
+#define adl__oct__var_id__hpp__
+
+#include "adl.cfg.hpp"
+#include "adl/oct.fwd.hpp"
+
+
+//
+// [[ API ]]
+//
+adl_BEGIN_MAIN_MODULE(oct)
+
+
+
+adl_END_MAIN_MODULE
+
+#endif // adl__oct__var_id__hpp__
