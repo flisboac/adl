d7b2846bcca02b2c2e3e8f374d4cfd2dc465334a
Flávio Lisbôa <flavio.lisboa@fasolti.com.br>
2018-01-30 21:53:55 -0200

    feat: #52

--- include/adl/oct/cl/context.hpp
+++ include/adl/oct/cl/context.hpp
@@ -21,19 +21,74 @@ namespace cl {
 
 class adl_CLASS context {
 public:
-    context() = delete;
+    context() = default;
     context(context const&) = delete;
     context(context &&) noexcept = default;
     context& operator=(context const&) = delete;
     context& operator=(context &&) noexcept = default;
+    ~context();
 
+    explicit context(::cl_context context);
     explicit context(::cl::Context& context);
 
     static context make();
+    static context make(adl::cl::errc& err);
 
 private:
-    ::cl::Context context_;
+    ::cl_context context_ = nullptr;
 };
+
+} // namespace cl
+
+adl_END_MAIN_MODULE
+
+
+/*
+ * [[ IMPLEMENTATION ]]
+ */
+adl_BEGIN_MAIN_MODULE(oct)
+
+namespace cl {
+
+adl_IMPL context::context(::cl_context context) : context_(context) { clRetainContext(context_); }
+adl_IMPL context::context(::cl::Context& context) : context(context.get()) {}
+adl_IMPL context::~context() { clReleaseContext(context_); }
+
+adl_IMPL context context::make() {
+    adl::cl::errc err;
+    context ctx = make(err);
+    if (!err) {
+        throw std::system_error(err);
+    }
+    return ctx;
+}
+
+adl_IMPL context context::make(adl::cl::errc& err) {
+    cl_platform_id platform_id = nullptr;
+    cl_device_id device_id = nullptr;
+    cl_context cl_ctx = nullptr;
+
+    int ret = clGetPlatformIDs(1, &platform_id, nullptr);
+
+    if (CL_SUCCESS != ret) {
+        ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, nullptr);
+    }
+
+    if (CL_SUCCESS != ret) {
+        cl_ctx = clCreateContext(nullptr, 1, &device_id, nullptr, nullptr, &ret);
+    }
+
+    err = static_cast<adl::cl::errc>(ret);
+
+    if (CL_SUCCESS != ret) {
+        context ctx(cl_ctx);
+        clReleaseContext(cl_ctx);
+        return ctx;
+
+    } else {
+        return {};
+    }
+}
 
 } // namespace cl
 
