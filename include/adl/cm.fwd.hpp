cb1bc02f262f78ea120d0baa2916100848210e07
Flávio Lisbôa <flavio.lisboa@fasolti.com.br>
2018-02-16 21:02:03 -0200

    feat: #52 - Some new ideas for the computation model

--- include/adl/cm.fwd.hpp
+++ include/adl/cm.fwd.hpp
@@ -28,14 +28,101 @@ enum class task_timing {
     execution // Time elapsed in between oper_state::started and oper_state::finished (including "teardown" time, if any).
 };
 
-template <typename BackendType> class basic_scheduler;
-template <typename ConstantType, typename BackendType, typename AllocatorType = std::allocator<ConstantType>> class basic_vector;
-template <typename SubClass, typename ReturnType, typename BackendType> class basic_task_template;
+enum class vector_dirty_flag {
+    none,
+    host,
+    device,
+    queued
+};
+
+#if 0
+// Notes:
+// -
+// NOTE: "host*" and "device*" must never be flagged at the same time. When "host*"
+enum class vector_dirty {
+    /* Vector is not dirty. The contents on both host and scheduler's device(s) are the same.
+     */
+    none,
+
+    /** Vector was marked dirty on CPU.
+     *
+     * It probably has modifications on the host not committed to the device(s).
+     * Automatically marked by the paged_vector on any write operation.
+     *
+     * A memory write will automatically be issued BEFORE enqueuing any device operation (e.g. kernel execution).
+     *
+     * After marked dirty on host, any subsequent host-dirty marks will be ignored until a flush operation is issued,
+     * occasion on which the vector will be marked host-flushing.
+     */
+    host,
+
+    /** Vector was marked dirty on device(s).
+     *
+     * The vector will be marked device-dirty right after enqueuing a device kernel execution. The mark will always be
+     * accompanied by the last (or all, in the case of out-of-order queues) kernel events.
+     *
+     * The vector will store all kernel-enqueue events until a flush or host memory operation is issued. When
+     * that happens, a device-read command will be issued, and all the kernel events collected so far will used as
+     * the read operation's synchronization point (e.g. all of them will be waited for). After issuing the read
+     * operation, the vector will be marked device-flushing.
+     */
+    device,
+
+    /** The host is dirty, but a device-write was already enqueued.
+     *
+     * The event will be stored in the scheduler, so that it can be used as a synchronization point for further kernel
+     * enqueues.
+     *
+     * Any subsequent writes on host or any operations on device MUST wait for the scheduler to process the write
+     * event. More specifically, while the vector is in the host-flushing state:
+     * - When a host memory operation is issued, the vector will force a synchronization on the host BEFORE performing
+     *   the operation. This is important because the device-write may happen at any time after the enqueuing.
+     * - When a device operation is issued, the scheduler will mark
+     *
+     * After processing:
+     * - If the write operation succeeded, the vector will be marked clean.
+     * - If the write operation failed, the vector will be marked dirty on host.
+     */
+    host_flushing,
+
+    /** The device is dirty, but a device-read was already enqueued.
+     *
+     * The event will be stored in the scheduler, so that it can be used as a synchronization point for further kernel
+     * enqueues.
+     *
+     */
+    device_flushing
+};
+#endif
+
+enum class vector_state {
+    clean,
+    dirty,
+    flushing
+};
+
 
 template <typename BackendType> class backend_traits;
+// class backend; // Not a template!
+
+// Uses std::allocator_traits, may do different things for different backends (e.g. OpenCL's SVM)
+template <typename ConstantType, typename BackendType> class basic_allocator;
+
 template <typename SchedulerType> class scheduler_traits;
+template <typename BackendType> class basic_scheduler;
+template <typename SubClass, typename BackendType> class basic_scheduler_template;
+
 template <typename VectorType> class vector_traits;
-template <typename TaskType> class task_traits;
+template <typename ConstantType, typename BackendType, typename AllocatorType = basic_allocator<ConstantType, BackendType>> class basic_vector;
+template <typename SubClass, typename ConstantType, typename BackendType, typename AllocatorType = basic_allocator<ConstantType, BackendType>> class basic_vector_template;
+
+template <typename VectorType> class task_traits;
+//template <typename ReturnType, typename BackendType> class basic_task;
+//template <typename ReturnType, typename BackendType, typename VectorType> class basic_vector_task;
+template <typename SubClass, typename ReturnType, typename BackendType> class basic_task_template;
+template <typename SubClass, typename ReturnType, typename BackendType, typename VectorType> class basic_vector_task_template;
+template <typename TaskType, typename ReturnType = typename task_traits<TaskType>::return_type, typename BackendType = typename task_traits<TaskType>::backend_type> class basic_job;
+template <typename SubClass, typename BackendType, typename TaskType> class basic_job_template;
 
 
 adl_END_ROOT_MODULE
